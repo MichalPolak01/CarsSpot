392f504a8b62b8fd7be3efe80198b7ba78b76ec7
MichalPolak01 <michalpl618@gmail.com>
2024-09-11 20:28:08 +0200

    Create carousel section

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,5 @@
 
+/* Mobile nav */
 const mobile_nav = document.getElementById('mobile_nav');
 const mobile_menu = document.getElementById('mobile-menu');
 const close_mobile_menu = document.getElementById('close-mobile-menu')
@@ -13,10 +14,30 @@ close_mobile_menu.addEventListener("click", (event) => {
     mobile_menu.classList.add('hidden');
 });
 
-// Initialization for ES Users
-import {
-    Carousel,
-    initTWE,
-  } from "tw-elements";
-  
-  initTWE({ Carousel });
+
+/* Carousel */
+const carousel = document.getElementById('carousel');
+let currentIndex = 0;
+
+function showSlide(index) {
+  const offset = -index * 40;
+  carousel.style.transform = `translateX(${offset}%)`;
+  updateDots(index);
+}
+
+function updateDots(index) {
+  const dots = document.querySelectorAll('.dot');
+  dots.forEach(dot => dot.classList.remove('bg-primary-blue'));
+  dots.forEach(dot => dot.classList.add('bg-gray-400'));
+
+  const activeDot = document.getElementById(`dot${index}`);
+  activeDot.classList.remove('bg-gray-400');
+  activeDot.classList.add('bg-primary-blue');
+}
+
+function goToSlide(index) {
+  currentIndex = index;
+  showSlide(index);
+}
+
+showSlide(currentIndex);
